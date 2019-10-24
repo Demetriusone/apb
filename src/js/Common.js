8e28a798313de6cd3d09af1b0b1e757c56dabcec
Demetriusone <wh46tesmoke@gmail.com>
2019-10-24 20:22:22 +0300

    ver0.1

---
+++ src/js/Common.js
@@ -0,0 +1,35 @@
+import objectFitImages from 'object-fit-images';
+import { $body, detectIE, $scrolledElements } from './_helpers';
+
+import './components/Header';
+import './components/Sliders';
+
+export class Common {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    objectFitImages();
+    this.addClassIE();
+    this.scrollBtn();
+  }
+
+  addClassIE() {
+    if (detectIE()) $body.addClass('is-ie');
+  }
+
+  scrollBtn () {
+    const $btn = $('.js-scroll-to');
+    const $destination = $('.js-scroll-dest');
+
+    $btn.on('click', (e) => {
+      e.preventDefault();
+      $scrolledElements.animate({
+        scrollTop: $destination.offset().top
+      }, 1500);
+    });
+  }
+}
+
+export default new Common();
